06ad362def91d85a6f5f3a53b9519bb4603e806e
= <=>
2024-10-01 00:35:43 +0300

    .

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,27 +1,44 @@
+from contextlib import asynccontextmanager
+import time
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, APIRouter, UploadFile, BackgroundTasks
+from navec import Navec
+from opinion_service.controllers import get_keywords
 
-from routes import main_router
 
-def start_app():
+MODELS = {}
 
-    app = FastAPI()
-    app.include_router(main_router)
-    app.middleware(
-        CORSMiddleware(app,
-        allow_origins=[
-            "http://localhost:3000"
-        ],
-        allow_credentials=True,
-        allow_methods="*",
-        allow_headers="*"
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    path = 'opinion_service/navec_hudlit_v1_12B_500K_300d_100q.tar'
+    navec_model = Navec.load(path)
+    MODELS["navec"] = navec_model
+    yield
 
-    ))
+def start_app(debug):
+
+    app = FastAPI(lifespan=lifespan, debug=debug)
+    opinion_router = APIRouter()
+
+    @opinion_router.get("/")
+    async def index():
+        return {"Message": "page of opinions"}
+
+    @opinion_router.post("/upload_opinion/")
+    async def upload_opinion_file(file: UploadFile, background_tasks: BackgroundTasks):
+        start = time.time()
+        content = await file.read()
+        # background_tasks.add_task(get_keywords, content, navec)
+        ans = await get_keywords(content, MODELS["navec"])
+        return {"result": time.time() - start, "ans": ans}
+
+    app.include_router(opinion_router)
     return app
 
+
+
 if __name__ == "__main__":
-    app = start_app()
+    app = start_app(True)
     uvicorn.run(app=app, host='127.0.0.1', port=8000)
 
 
--- backend/src/opinion_service/views.py
+++ backend/src/opinion_service/views.py
@@ -1,19 +1,21 @@
+import os
 import time
-from fastapi import APIRouter, UploadFile, BackgroundTasks
+from fastapi import APIRouter, UploadFile
+from navec import Navec
 
 from .controllers import get_keywords
+from routes import main_router as opinion_router
+from main import MODELS, main_router as opinion_router
 
-
-router = APIRouter(prefix="/opinions")
-
-
-@router.get("/")
+opinion_router
+@opinion_router.get("/")
 async def index():
     return {"Message": "page of opinions"}
 
-@router.post("/upload_opinion/")
+@opinion_router.post("/upload_opinion/")
 async def upload_opinion_file(file: UploadFile, background_tasks: BackgroundTasks):
     start = time.time()
-    content = file.file
-    background_tasks.add_task(get_keywords, content)
-    return {"result": time.time() - start}
+    content = await file.read()
+    # background_tasks.add_task(get_keywords, content, navec)
+    ans = await get_keywords(content, MODELS["navec"])
+    return {"result": time.time() - start, "ans": ans}
